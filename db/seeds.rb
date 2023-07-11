5f9a3cc534289cecb40873eb9bbda7d3fa06c435
Dylan Timmons <dylanstimmons@icloud.com>
2023-07-11 16:29:23 -0600

    update to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,11 @@
 shuttle_1 = Shuttle.create(name: "Columbia", in_service: false, year_built: 1975)
 shuttle_1.astronauts.create(name: "Joe Engle", age: 45, space_veteran: false)
 shuttle_1.astronauts.create(name: "Richard Truly", age: 46, space_veteran: true)
+
+shuttle_2 = Shuttle.create(name: "Discovery", in_service: false, year_built: 1985)
+shuttle_2.astronauts.create(name: "Henry Hartsfield", age: 45, space_veteran: true)
+shuttle_2.astronauts.create(name: "Judith Resnik", age: 35, space_veteran: false)
+
+shuttle_3 = Shuttle.create(name: "DragonX", in_service: true, year_built: 2010)
+shuttle_3.astronauts.create(name: "Douglas Hurley", age: 54, space_veteran: true)
+shuttle_3.astronauts.create(name: "Robert Behnken", age: 50, space_veteran: true)
